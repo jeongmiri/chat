b97cae81d6514278c333c05725bc971dee18c8c4
jeongmiri <alfl2484>
2022-11-28 13:15:21 +0900

    init: 프로젝트 세팅

---
+++ config/routing.py
@@ -0,0 +1,12 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from apps import routing
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
