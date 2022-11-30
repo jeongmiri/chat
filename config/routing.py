6332196bee497481d23a564f11c928623de9862d
jeongmiri <alfl2484>
2022-11-30 13:36:19 +0900

    feat: 비동기로 websocket 연결 처리

--- config/routing.py
+++ config/routing.py
@@ -1,7 +1,7 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-from apps import routing
+from apps.base import routing
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
